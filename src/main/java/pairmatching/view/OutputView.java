4964871a72749022782332acee0e090bd5872b72
namtndus <rnrmf0721@gmail.com>
2022-11-27 14:13:27 +0900

    feat(시작 페이지 기능 구현):
    시작문구 와 질문을 물어보는 함수 구현

---
+++ src/main/java/pairmatching/view/OutputView.java
@@ -0,0 +1,15 @@
+package pairmatching.view;
+
+import static pairmatching.model.StartMessageConst.*;
+
+public class OutputView {
+
+    public void startMessage(){
+        System.out.println(START_MESSAGE);
+        System.out.println(FIRST_LOGIC);
+        System.out.println(SECOND_LOGIC);
+        System.out.println(THIRD_LOGIC);
+        System.out.println(EXIT_LOGIC);
+    }
+
+}
