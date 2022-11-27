1e9747cb895d76b28b5879a7e9bf60f26db2b938
namtndus <rnrmf0721@gmail.com>
2022-11-27 15:13:12 +0900

    feat: 사용자가 '1. 페어 매칭'를 선택했을 때 나오는 메세지 구현

--- src/main/java/pairmatching/view/OutputView.java
+++ src/main/java/pairmatching/view/OutputView.java
@@ -1,5 +1,6 @@
 package pairmatching.view;
 
+import static pairmatching.model.PairMatchingMessageConst.*;
 import static pairmatching.model.StartMessageConst.*;
 
 public class OutputView {
@@ -12,4 +13,12 @@ public class OutputView {
         System.out.println(EXIT_LOGIC);
     }
 
+    public void printPairMatcingMessage(){
+        System.out.println(VERTICAL_BAR);
+        System.out.println(TYPE_OF_COURSE);
+        System.out.println(TYPE_OF_LEVELS);
+        System.out.println(VERTICAL_BAR);
+        System.out.println(QUESTION);
+        System.out.println(EXAMPLE_MESSAGE);
+    }
 }
